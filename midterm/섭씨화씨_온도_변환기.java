9e6b41078644d5b9b005ec39486a713080be53f1
JerryK <kskyung0624@gmail.com>
2021-04-16 08:17:42 +0900

    노력상, 섭씨화씨 온도 변환기 통과

---
+++ midterm/섭씨화씨_온도_변환기.java
@@ -0,0 +1,54 @@
+package midterm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+interface Temperature {
+    public double getCelsius();
+
+    public double getFahrenheit();
+
+    public void setCelsius(double celsius);
+
+    public void setFahrenheit(double fahrenheit);
+}
+
+public class 섭씨화씨_온도_변환기 implements Temperature {
+    double cel = 0;
+    double fah = 0;
+
+    public double getCelsius() {
+        return this.cel;
+    }
+
+    public double getFahrenheit() {
+        return this.fah;
+    }
+
+    public void setCelsius(double celsius) {
+        this.fah = Math.round((9 * celsius / 5 + 32) * 100) / 100.0;
+    }
+
+    public void setFahrenheit(double fahrenheit) {
+        this.cel = Math.round(5 * (fahrenheit - 32) / 9 * 100) / 100.0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        섭씨화씨_온도_변환기 c = new 섭씨화씨_온도_변환기();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            if(input[1].equals("C")) {
+                c.setCelsius(Double.parseDouble(input[0]));
+                System.out.println(c.getFahrenheit());
+            }
+            else {
+                c.setFahrenheit(Double.parseDouble(input[0]));
+                System.out.println(c.getCelsius());
+            }
+        }
+    }
+}
