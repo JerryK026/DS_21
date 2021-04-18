44ed3026baf49bc13a3cac1bbc8464e5db7aa5d2
JerryK <kskyung0624@gmail.com>
2021-04-18 22:00:15 +0900

    학생 명단 관리

--- midterm/부분합_1D.java
+++ midterm/부분합_1D.java
@@ -1,2 +1,27 @@
-package midterm;public class 부분합_1D {
+package midterm;
+
+import java.util.*;
+import java.io.*;
+
+public class 부분합_1D {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] num = new int[N];
+        int[] sum = new int[N + 1];
+
+        for(int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+            sum[i + 1] = sum[i] + num[i];
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            System.out.println(-(sum[Integer.parseInt(st.nextToken()) - 1]) + sum[Integer.parseInt(st.nextToken())]);
+        }
+    }
 }
